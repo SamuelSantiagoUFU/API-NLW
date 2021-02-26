668d00be1e17ce3c76bba787a8379773a6e43ac3
Samuel <samuel.santiago@ufu.br>
2021-02-26 18:50:17 -0300

    Tratando erros

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,9 +2,10 @@ import { Connection, createConnection, getConnectionOptions } from "typeorm"
 
 export default async (): Promise<Connection> => {
 	const defaultOptions = await getConnectionOptions()
+	
 	return createConnection(
 		Object.assign(defaultOptions, {
-			database: process.env.NODE_ENV === "test" ? "./src/database/database.test.sqlite" : defaultOptions.database
+			database: process.env.NODE_ENV === "'test'" ? "./src/database/database.test.sqlite" : defaultOptions.database
 		})
 	)
 }
